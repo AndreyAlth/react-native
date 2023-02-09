348b935f960c07dbe92c57fdbb402fd272b37cb6
AndreyAlth <andreyalth@gmail.com>
2023-02-09 01:14:12 -0600

    agregar peticiones http

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 // import TiposBasicos from "./typescript/TiposBasicos"
 
+import { Usuarios } from "./components/Usuarios"
+
 // import { Contador } from "./components/Contador";
-import { Login } from "./components/Login";
+// import { Login } from "./components/Login";
 
 // import ObjetosLiterales from "./typescript/ObjetosLiterales"
 // import { Funciones } from "./typescript/Funciones"
 
 const App = () => {
   return (
-    <div className="mt-2">
+    <div className='mt-2'>
       <h1> Introducción a TS - React</h1>
       <hr />
       {/* <TiposBasicos/> */}
       {/* <ObjetosLiterales /> */}
       {/* <Funciones/> */}
       {/* <Contador /> */}
-      <Login/>
+      {/* <Login/> */}
+      <Usuarios />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
