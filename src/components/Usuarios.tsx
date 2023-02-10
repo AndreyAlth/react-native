9cf330f3f105cfd17261af49b03b13450861c769
AndreyAlth <andreyalth@gmail.com>
2023-02-10 02:19:08 -0600

    mostrar usuarios

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,14 +1,43 @@
-import { useEffect } from 'react';
-import { reqRes } from '../api/reqRes';
+import { useEffect, useState } from "react"
+import { reqRes } from "../api/reqRes"
+import { ReqResListado, Usuario } from "../interfaces/reqRes"
 export const Usuarios = () => {
-    useEffect(() => {
-        reqRes.get('/users')
-        .then(resp => {
-            console.log(resp.data.data)
-        })
-        .catch (console.log)
-    }, [])
-    
+  const [usuarios, setUsuarios] = useState<Usuario[]>([])
+
+  useEffect(() => {
+    reqRes
+      .get<ReqResListado>("/users")
+      .then((resp) => {
+        console.log(resp.data.data)
+        setUsuarios(resp.data.data)
+      })
+      .catch(console.log)
+  }, [])
+
+  const renderItem = ({
+    id,
+    first_name,
+    last_name,
+    email,
+    avatar,
+  }: Usuario) => {
+    return (
+      <tr key={id.toLocaleString()}>
+        <td>
+          <img
+            src={avatar}
+            alt={first_name}
+            style={{ width: 50, borderRadius: 100 }}
+          />
+        </td>
+        <td>
+          {first_name} {last_name}
+        </td>
+        <td>{email}</td>
+      </tr>
+    )
+  }
+
   return (
     <>
       <h3>Usuarios</h3>
@@ -20,8 +49,11 @@ export const Usuarios = () => {
             <th>email</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>{usuarios.map((usuario) => renderItem(usuario))}</tbody>
       </table>
+      <button className="btn btn-primary">
+        Siguientes
+      </button>
     </>
   )
 }
